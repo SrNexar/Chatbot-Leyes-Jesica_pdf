fe1bb9a7a0ce2f2a99439aff7b0bb4da65c72711
Jesdhy <pinchajessy529@gmail.com>
2025-07-17 19:34:53 -0500

    refactor: :construction: Se añade un nuevo campo
    Añdi el id del denunciante

--- schemas/casoCopy.py
+++ schemas/casoCopy.py
@@ -2,14 +2,15 @@ from pydantic import BaseModel, HttpUrl
 from typing import Optional, List
 
 class CasoJudicial(BaseModel):
+    denunciante_id: str  
     device_id: str
     device_type: str
     location: str
     alert_information: str
     latitude: float
     longitude: float
-    date: str  
-    time: str  
+    date: str
+    time: str
     stream_url: HttpUrl
     transcription_video: str
     transcription_audio: Optional[str]
